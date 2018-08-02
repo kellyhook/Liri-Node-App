4848d6af1236f02df5e7f033529411fbba7f658e
Kelly Hook <kellymhook@SpaceComputer.local>
2018-08-02 16:27:52 -0700

    all packages installed

--- liri.js
+++ liri.js
@@ -1,221 +1,268 @@
-var keys = require("./keys.js");
+require("dotenv").config();
+
+
+// Required NPM modules and files.
+// ____________________________________________________________________________________
+
+// NPM module used to access Twitter API.
+var Twitter = require("twitter");
+
+// Used to access Twitter keys in local file, keys.js.
+var twitterKeysFile = require("./keys.js");
+
+// NPM module used to access Spotify API.
 var spotify = require("spotify");
-var twitter = require("twitter");
-// var tmdb = require("tmdb").init({apikey:"798d16e44bf2357493e013567298dcd5"});
-// var TmdbApi = require("tmdb-api");
+
+// NPM module used to access OMDB API.
 var request = require("request");
+
+// NPM module used to read the random.txt file.
 var fs = require("fs");
 
+// Output file for logs.
+var filename = './log.txt';
 
-// console.log(twitterKeys);
+// NPM module used for logging solution.
+var log = require('simple-node-logger').createSimpleFileLogger(filename);
 
-var nodeArgs = process.argv;
-var query = [];
-// var action = process.argv.slice(2);
+// All log information printed to log.txt.
+log.setLevel('all');
 
-for (var i = 2; i < nodeArgs.length; i++) {
+// Controller and required parameters.
+// ____________________________________________________________________________________
 
-  query.push(nodeArgs[i]);
 
+// Action requested.
+var action = process.argv[2];
+
+// Optional argument to request specific information.
+// Based on action type.
+var argument = "";
+
+// Controller function that determines what action is taken,
+// and specific data to complete that action.
+doSomething(action, argument);
+
+// Switch operation used to determin which action to take.
+function doSomething(action, argument) {
+
+  /* Controls optional third argument.
+  Defines specific data relating to the action.
+  For example, when requesting song information,
+  you can pass in a song title.
+  */
+  argument = getThirdArgument();
+
+  switch (action) {
+
+    // Gets list of tweets.
+    case "my-tweets":
+      getMyTweets();
+      break;
+
+      // Gets song information.
+    case "spotify-this-song":
+
+      // First gets song title argument.
+      var songTitle = argument;
+
+      // If no song title provided, defaults to specific song.
+      if (songTitle === "") {
+        lookupSpecificSong();
+
+        // Else looks up song based on song title.
+      } else {
+        // Get song information from Spotify.
+        getSongInfo(songTitle);
+      }
+      break;
+
+      // Gets movie information.
+    case "movie-this":
+
+      // First gets movie title argument.
+      var movieTitle = argument;
+
+      // If no movie title provided, defaults to specific movie.
+      if (movieTitle === "") {
+        getMovieInfo("Mr. Nobody");
+
+        // Else looks up song based on movie title.
+      } else {
+        getMovieInfo(movieTitle);
+      }
+      break;
+
+      // Gets text inside file, and uses it to do something.
+    case "do-what-it-says":
+      doWhatItSays();
+      break;
+  }
 }
 
-var argOne = query.splice(0, 1);
-var argTwo = query.join(" ");
-var action = String(argOne);
-var value = String(argTwo);
+// Returns optional third argument, for example,
+// when requesting song information, include a song title.
+function getThirdArgument() {
 
-console.log("Searching for " + value);
-console.log("What command? " + action);
+  // Stores all possible arguments in array.
+  argumentArray = process.argv;
 
-
-switch (action) {
-  case "my-tweets":
-    myTweets();
-    logAction();
-    break;
-
-  case "spotify-this-song":
-    spotifyThisSong();
-    logAction();
-    break;
-
-  case "movie-this":
-    movieThis();
-    logAction();
-    break;
-
-  case "do-what-it-says":
-    doThis();
-    logAction();
-    break;
-
+  // Loops through words in node argument.
+  for (var i = 3; i < argumentArray.length; i++) {
+    argument += argumentArray[i];
+  }
+  return argument;
 }
 
-//Functions
+// Function to show my last 20 tweets.
+function getMyTweets() {
 
-//Commands for Liri to take in...
-// * `my-tweets`
-function myTweets() {
+  // Passes Twitter keys into call to Twitter API.
+  var client = new Twitter(twitterKeysFile.twitterKeys);
 
-  var twitterKeys = keys.twitterKeys;
-
-  var client = new twitter({
-    consumer_key: twitterKeys.consumer_key,
-    consumer_secret: twitterKeys.consumer_secret,
-    access_token_key: twitterKeys.access_token_key,
-    access_token_secret: twitterKeys.access_token_secret
-  });
-
+  // Search parameters includes my tweets up to last 20 tweets;
   var params = {
-    screen_name: "jlp0328",
+    q: '@MegginKearney',
     count: 20
   };
 
-  client.get("statuses/user_timeline", params, function(error, tweets, response) {
-    if (error) {
-      console.log(error);
-    }
+  // Shows up to last 20 tweets and when created in terminal.
+  client.get('search/tweets', params, function(error, tweets, response) {
+    if (!error) {
 
-    for (var i = 0; i < tweets.length; i++) {
-      console.log("************");
-      console.log(tweets[i].text);
-      console.log("************");
-    }
-
-  });
-}
-
-// * `spotify-this-song`
-function spotifyThisSong() {
-
-  spotify.search({
-    type: "track",
-    query: value
-  }, function(err, data) {
-
-    if (err) {
-      console.log("Error occurred: " + err);
-      return;
-    }
-    // * if no song is provided then your program will default to
-    //   * "The Sign" by Ace of Base
-    if (value === "") {
-      console.log("************");
-      console.log("Artist: Ace of Base");
-      console.log("Song: The Sign");
-      console.log("Song Link: https://open.spotify.com/track/0hrBpAOgrt8RXigk83LLNE");
-      console.log("Album: The Sign");
-      console.log("************");
-    } else {
-
-      for (i = 0; i < 5; i++) {
-
-        var results = data.tracks.items[i];
-
-        var artist = results.artists[0].name;
-        var songName = results.name;
-        var songLink = results.external_urls.spotify;
-        var album = results.album.name;
-
-        //Need: artist(s), song's name, preview link of song, album//
-        console.log("************");
-        console.log("Artist: " + artist);
-        console.log("Song: " + songName);
-        console.log("Song Link: " + songLink);
-        console.log("Album: " + album);
-        console.log("************");
+      // Loops through tweets and prints out tweet text and creation date.
+      for (var i = 0; i < tweets.statuses.length; i++) {
+        var tweetText = tweets.statuses[i].text;
+        logOutput("Tweet Text: " + tweetText);
+        var tweetCreationDate = tweets.statuses[i].created_at;
+        logOutput("Tweet Creation Date: " + tweetCreationDate);
       }
-    }
-
-  });
-
-}
-
-// * `movie-this`
-function movieThis() {
-
-  var queryURL = "https://api.themoviedb.org/3/search/movie?api_key=798d16e44bf2357493e013567298dcd5&query=" + value;
-
-  request(queryURL, function(error, response, body) {
-
-    // If the request is successful (i.e. if the response status code is 200)
-    if (error) {
-      console.log("Error occurred: " + error);
-      return;
-    }
-
-    if (value === "") {
-
-      console.log("************");
-      console.log("Movie Name: Mr.Nobody");
-      console.log("Release Date: 2009-09-11");
-      console.log("Synopsis: Nemo Nobody leads an ordinary existence with his wife and 3 children; one day, he wakes up as a mortal centenarian in the year 2092.");
-      console.log("Average Vote: 7.9");
-      console.log("Language: en");
-      console.log("************");
-
     } else {
-      console.log("************");
-      console.log("Movie Name: " + JSON.parse(body).results[0].title);
-      console.log("Release Date: " + JSON.parse(body).results[0].release_date);
-      console.log("Synopsis: " + JSON.parse(body).results[0].overview);
-      console.log("Average Vote: " + JSON.parse(body).results[0].vote_average);
-      console.log("Language: " + JSON.parse(body).results[0].original_language);
-      console.log("************");
+      logOutput(error);
     }
   });
-
 }
 
-// * `do-what-it-says`
-function doThis() {
+// Calls Spotify API to retrieve song information for song title.
+function getSongInfo(songTitle) {
 
-  // Feel free to change the text in that document to test out the feature for other commands.
-  fs.readFile("random.txt", "utf8", function(error, data) {
-
-    var content = data.split(",");
-
-    // var array = data.toString().split("\n");
-    // console.log(array);
-
-    action = content[0];
-    value = content[1];
-
-    switch (action) {
-      case "my-tweets":
-        myTweets();
-        break;
-
-      case "spotify-this-song":
-        spotifyThisSong();
-        break;
-
-      case "movie-this":
-        movieThis();
-        break;
-
-      case "do-what-it-says":
-        doThis();
-        break;
-
-    }
-
-  });
-
-}
-
-function logAction() {
-
-  var logItem = "\nSearch String:" + action + "," + value;
-  console.log(logItem);
-
-  fs.appendFile("log.txt", logItem, function(err) {
-
+  // Calls Spotify API to retrieve a track.
+  spotify.search({
+    type: 'track',
+    query: songTitle
+  }, function(err, data) {
     if (err) {
-      console.log(err);
-    } else {
-      console.log("Content Added!");
+      logOutput.error(err);
+      return
     }
 
+    /* The Spotify node module defaults to 20 no matter what.
+    Attempted to add a limit, which seems to do nothing.
+    Homework requirements suggest we should only return one song.
+    Used array properties to retrict songs returns.
+    There could very well be a better way to do this.
+    But it's as close to requirements I could get using Spotify module.
+    */
+    var artistsArray = data.tracks.items[0].album.artists;
+
+    // Array to hold artist names, when more than one artist exists for a song.
+    var artistsNames = [];
+
+    // Pushes artists for track to array.
+    for (var i = 0; i < artistsArray.length; i++) {
+      artistsNames.push(artistsArray[i].name);
+    }
+
+    // Converts artists array to string, and makes it pretty.
+    var artists = artistsNames.join(", ");
+
+    // Prints the artist(s), track name, preview url, and album name.
+    logOutput("Artist(s): " + artists);
+    logOutput("Song: " + data.tracks.items[0].name)
+    logOutput("Spotify Preview URL: " + data.tracks.items[0].preview_url)
+    logOutput("Album Name: " + data.tracks.items[0].album.name);
   });
+
+}
+
+// When no song title provided, defaults to specific song, The Sign.
+function lookupSpecificSong() {
+
+  // Calls Spotify API to retrieve a specific track, The Sign, Ace of Base.
+  spotify.lookup({
+    type: 'track',
+    id: '3DYVWvPh3kGwPasp7yjahc'
+  }, function(err, data) {
+    if (err) {
+      logOutput.error(err);
+      return
+    }
+
+    // Prints the artist, track name, preview url, and album name.
+    logOutput("Artist: " + data.artists[0].name);
+    logOutput("Song: " + data.name);
+    logOutput("Spotify Preview URL: " + data.preview_url);
+    logOutput("Album Name: " + data.album.name);
+  });
+}
+
+// Passes a query URL to OMDB to retrieve movie information for movie title.
+// If no movie title provided, defaults to the movie, Mr. Nobody.
+function getMovieInfo(movieTitle) {
+
+  // Runs a request to the OMDB API with the movie specified.
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&tomatoes=true&r=json";
+
+  request(queryUrl, function(error, response, body) {
+    // If the request is successful...
+    if (!error && response.statusCode === 200) {
+
+      // Parses the body of the site and recovers movie info.
+      var movie = JSON.parse(body);
+
+      // Prints out movie info.
+      logOutput("Movie Title: " + movie.Title);
+      logOutput("Release Year: " + movie.Year);
+      logOutput("IMDB Rating: " + movie.imdbRating);
+      logOutput("Country Produced In: " + movie.Country);
+      logOutput("Language: " + movie.Language);
+      logOutput("Plot: " + movie.Plot);
+      logOutput("Actors: " + movie.Actors);
+
+      // Had to set to array value, as there seems to be a bug in API response,
+      // that always returns N/A for movie.tomatoRating.
+      logOutput("Rotten Tomatoes Rating: " + movie.Ratings[2].Value);
+      logOutput("Rotten Tomatoes URL: " + movie.tomatoURL);
+    }
+  });
+}
+
+// Uses fs node package to take the text inside random.txt,
+// and do something with it.
+function doWhatItSays() {
+
+  fs.readFile("random.txt", "utf8", function(err, data) {
+    if (err) {
+      logOutput.error(err);
+    } else {
+
+      // Creates array with data.
+      var randomArray = data.split(",");
+
+      // Sets action to first item in array.
+      action = randomArray[0];
+
+      // Sets optional third argument to second item in array.
+      argument = randomArray[1];
+
+      // Calls main controller to do something based on action and argument.
+      doSomething(action, argument);
+    }
+  });
+}
+
+// Logs data to the terminal and output to a text file.
+function logOutput(logText) {
+  log.info(logText);
+  console.log(logText);
 }
